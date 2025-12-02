60f24294b2ed07b12b34dbb2f8a8fa03c776b66b
c0der <raphee.rattanamanoonporn@gmail.com>
2025-12-02 19:10:00 +0700

    Add files via upload

---
+++ robot_code2.ino
@@ -0,0 +1,26 @@
+#include "src/Motor.h"
+#define DirectionPin 4
+#define BaudRate 115200
+
+int theta = 360;
+int t = 20000;
+int wheelr = 3; //C
+float innerr = 22.5; //C
+float outerr = 37.5; //C
+
+int innerrpm = (theta*innerr*60000)/(3*t*360);
+int outerrpm = (theta*outterr*60000)/(3*t*360); 
+
+void setup() {
+  Serial.begin(115200);
+  Motor.begin(BaudRate, DirectionPin, &Serial2);
+  delay(10000); //Pre Launch
+  Motor.turnWheel(1, LEFT, innerrpm);
+  Motor.turnWheel(2, RIGHT, outerrpm);
+  delay(t);
+  Motor.turnWheel(1,0);
+  Motor.turnWheel(2,0); 
+}
+
+void loop() {
+}
